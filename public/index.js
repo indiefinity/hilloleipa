4c06001415234c0abb298ea50b6afe69c64c8b5c
viluzi <ville@kastell.tk>
2019-05-27 14:24:06 +0300

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,22 @@
+var red = 255
+var blue = 0
+var green = 0
+function lol() {
+    if (red > 0 && blue == 0) {
+        red--;
+        green++;
+    }
+    else if (green > 0) {
+        green--;
+        blue++;
+    }
+    else if (blue > 0) {
+        blue--;
+        red++;
+    }
+    document.body.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue +")";
+    setTimeout(function(){
+        lol();
+    }, 5);
+}
+lol()
